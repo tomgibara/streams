f644784995347e6b975adbdd342310276299dd35
Tom Gibara <me@tomgibara.com>
2015-10-13 16:25:27 +0100

    Renames default stream tests.

--- src/test/java/com/tomgibara/streams/DefaultStreamTest.java
+++ src/test/java/com/tomgibara/streams/DefaultStreamTest.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class AbstractStreamTest extends FuzzStreamTest {
+public class DefaultStreamTest extends FuzzStreamTest {
 
 	@Override
 	WriteStream newWriter() {
