722ad29345dbdf4ae1a7f3833fa5bdd586a3281f
Tom Gibara <me@tomgibara.com>
2015-10-13 16:40:15 +0100

    Adds convenient interfaces for the simple [de]serialization of objects.

---
+++ src/main/java/com/tomgibara/streams/StreamDeserializer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Tom Gibara
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.tomgibara.streams;
+
+/**
+ * Converts byte data into an object.
+ *
+ * @author Tom Gibara
+ *
+ * @param <T> the type of object that will be created from the data
+ */
+
+@FunctionalInterface
+public interface StreamDeserializer<T> {
+
+	/**
+	 * Called to deserialize a stream into an object instance.
+	 * 
+	 * @param stream
+	 *            the stream from which the object should be deserialized
+	 * @return the deserialized object
+	 */
+
+	T deserialize(WriteStream stream);
+
+}
