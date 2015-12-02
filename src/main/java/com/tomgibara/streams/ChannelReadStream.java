87346d1de018fc4cbce0f71289cb86b0809c81b3
Tom Gibara <me@tomgibara.com>
2015-12-02 12:58:18 +0000

    Removes abstract channel base class.

--- src/main/java/com/tomgibara/streams/ChannelReadStream.java
+++ src/main/java/com/tomgibara/streams/ChannelReadStream.java
@@ -21,7 +21,10 @@ import java.nio.channels.ReadableByteChannel;
  * @see EndOfStreamException#EOS
  */
 
-public final class ChannelReadStream extends AbstractChannelReadStream {
+public final class ChannelReadStream implements ReadStream {
+
+	private final ReadableByteChannel channel;
+	private final ByteBuffer buffer = ByteBuffer.allocate(8);
 
 	/**
 	 * Creates a stream that reads from the supplied channel. Bytes will be read
@@ -32,9 +35,77 @@ public final class ChannelReadStream extends AbstractChannelReadStream {
 	 */
 
 	public ChannelReadStream(ReadableByteChannel channel) {
-		super(channel);
+		if (channel == null) throw new IllegalArgumentException("null channel");
+		this.channel = channel;
+	}
+	
+	@Override
+	public byte readByte() throws StreamException {
+		return read(buffer, 7).get();
+	}
+	
+	@Override
+	public void readBytes(byte[] bs) throws StreamException {
+		fillBuffer(ByteBuffer.wrap(bs));
+	}
+	
+	@Override
+	public void readBytes(byte[] bs, int off, int len) throws StreamException {
+		fillBuffer(ByteBuffer.wrap(bs, off, len));
+	}
+	
+	@Override
+	public int readInt() throws StreamException {
+		return read(buffer, 4).getInt();
+	}
+	
+	@Override
+	public boolean readBoolean() throws StreamException {
+		return read(buffer, 7).get() != 0;
+	}
+	
+	@Override
+	public short readShort() throws StreamException {
+		return read(buffer, 6).getShort();
+	}
+	
+	@Override
+	public long readLong() throws StreamException {
+		return read(buffer, 0).getLong();
+	}
+	
+	@Override
+	public float readFloat() throws StreamException {
+		return read(buffer, 4).getFloat();
 	}
 
+	@Override
+	public double readDouble() throws StreamException {
+		return read(buffer, 0).getDouble();
+	}
+	
+	@Override
+	public char readChar() throws StreamException {
+		return read(buffer, 6).getChar();
+	}
+	
+	@Override
+	public StreamBuffering getBuffering() {
+		return StreamBuffering.PREFER_DIRECT;
+	}
+	
+	@Override
+	public void fillBuffer(ByteBuffer buffer) throws StreamException {
+		try {
+			while (buffer.hasRemaining()) {
+				int count = channel.read(buffer);
+				if (count == -1) return;
+			}
+		} catch (IOException e) {
+			throw new StreamException(e);
+		}
+	}
+	
 	/**
 	 * Creates a stream which reads from the underlying channel. Bytes will be
 	 * read starting from the current channel position. No more than
@@ -50,4 +121,39 @@ public final class ChannelReadStream extends AbstractChannelReadStream {
 	public ReadStream bounded(long length) {
 		return new ChannelReadStream(new BoundedReadableChannel(channel, length));
 	}
+
+	@Override
+	public ReadableByteChannel asChannel() {
+		return channel;
+	}
+
+	@Override
+	public InputStream asInputStream() {
+		return Channels.newInputStream(channel);
+	}
+	
+	/**
+	 * Closes the underlying channel.
+	 * 
+	 * @throws StreamException
+	 *             if an {@link IOException} occurs while closing the channel
+	 */
+	
+	@Override
+	public void close() {
+		try {
+			channel.close();
+		} catch (IOException e) {
+			throw new StreamException();
+		}
+	}
+	
+	private ByteBuffer read(ByteBuffer buffer, int position) {
+		buffer.position(position);
+		fillBuffer(buffer);
+		if (buffer.hasRemaining()) throw EndOfStreamException.EOS;
+		buffer.position(position);
+		return buffer;
+	}
+
 }
--- src/main/java/com/tomgibara/streams/ChannelWriteStream.java
+++ src/main/java/com/tomgibara/streams/ChannelWriteStream.java
@@ -1,7 +1,9 @@
 package com.tomgibara.streams;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.WritableByteChannel;
 
 /**
@@ -19,7 +21,10 @@ import java.nio.channels.WritableByteChannel;
  * @see EndOfStreamException#EOS
  */
 
-public class ChannelWriteStream extends AbstractChannelWriteStream {
+public final class ChannelWriteStream implements WriteStream {
+
+	private final WritableByteChannel channel;
+	private final ByteBuffer buffer = ByteBuffer.allocate(8);
 
 	/**
 	 * Creates a stream that writes to the supplied channel. Bytes will be
@@ -30,7 +35,69 @@ public class ChannelWriteStream extends AbstractChannelWriteStream {
 	 */
 
 	public ChannelWriteStream(WritableByteChannel channel) {
-		super(channel);
+		this.channel = channel;
+	}
+	
+	@Override
+	public void writeByte(byte v) throws StreamException {
+		write( buffer.put(v) );
+	}
+	
+	@Override
+	public void writeBytes(byte[] bs) throws StreamException {
+		drainBuffer(ByteBuffer.wrap(bs));
+	}
+	
+	@Override
+	public void writeBytes(byte[] bs, int off, int len) throws StreamException {
+		drainBuffer(ByteBuffer.wrap(bs, off, len));
+	}
+	
+	@Override
+	public void writeInt(int v) throws StreamException {
+		write( buffer.putInt(v) );
+	}
+	
+	@Override
+	public void writeBoolean(boolean v) throws StreamException {
+		write( buffer.put(v ? (byte) 1 : (byte) 0) );
+	}
+	
+	@Override
+	public void writeShort(short v) throws StreamException {
+		write( buffer.putShort(v) );
+	}
+	
+	@Override
+	public void writeLong(long v) throws StreamException {
+		write( buffer.putLong(v) );
+	}
+	
+	@Override
+	public void writeFloat(float v) throws StreamException {
+		write( buffer.putFloat(v) );
+	}
+	
+	@Override
+	public void writeDouble(double v) throws StreamException {
+		write( buffer.putDouble(v) );
+	}
+	
+	@Override
+	public void writeChar(char v) throws StreamException {
+		write( buffer.putChar(v) );
+	}
+	
+	@Override
+	public void drainBuffer(ByteBuffer buffer) throws StreamException {
+		try {
+			while (buffer.hasRemaining()) {
+				int count = channel.write(buffer);
+				if (count == -1) return;
+			}
+		} catch (IOException e) {
+			throw new StreamException(e);
+		}
 	}
 
 	/**
@@ -48,4 +115,39 @@ public class ChannelWriteStream extends AbstractChannelWriteStream {
 	public WriteStream bounded(long length) {
 		return new ChannelWriteStream(new BoundedWritableChannel(channel, length));
 	}
+
+	@Override
+	public OutputStream asOutputStream() {
+		return Channels.newOutputStream(channel);
+	}
+	
+	@Override
+	public WritableByteChannel asChannel() {
+		return channel;
+	}
+	
+	/**
+	 * Closes the underlying channel.
+	 * 
+	 * @throws StreamException
+	 *             if an {@link IOException} occurs while closing the channel
+	 */
+	
+	@Override
+	public void close() throws StreamException {
+		try {
+			channel.close();
+		} catch (IOException e) {
+			throw new StreamException();
+		}
+	}
+	
+	private void write(ByteBuffer buffer) {
+		buffer.flip();
+		drainBuffer(buffer);
+		boolean eos = buffer.hasRemaining();
+		buffer.clear();
+		if (eos) throw EndOfStreamException.EOS;
+	}
+
 }
